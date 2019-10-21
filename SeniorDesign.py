c66d8227f488e5711cbb157271094c3c1d0ccca4
collin-allen <42555339+collin-allen@users.noreply.github.com>
2019-10-20 22:37:33 -0500

    Added function to manage the color masks and clean up code

--- SeniorDesign.py
+++ SeniorDesign.py
@@ -15,38 +15,25 @@ cv.namedWindow("Display Window", cv.WINDOW_AUTOSIZE)
 # Convert BGR to HSV
 hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
 # define range of blue color in HSV
-delta = 2.5
-lower_green = np.array([50 - delta, 100, 100])
-upper_green = np.array([50 + delta, 255, 255])
 
-lower_blue = np.array([100 - delta, 100, 100])
-upper_blue = np.array([100 + delta, 255, 255])
+def set_masks(colors, delta):
+    bounds = [(np.array([color - delta, 100, 100]), np.array([color + delta, 255, 255])) for color in colors]
+    masks = [cv.inRange(hsv, bound[0], bound[1]) for bound in bounds]
+    mask = masks[0]
+    for i in range(1, len(masks)):
+        mask = mask | masks[i]
+    return mask
 
-lower_pink = np.array([166 - delta, 100, 100])
-upper_pink = np.array([166 + delta, 255, 255])
-
-lower_purple = np.array([130 - delta, 0, 0])
-upper_purple = np.array([130 + delta, 255, 255])
-
-lower_yellow = np.array([28 - delta, 0, 0])
-upper_yellow = np.array([28 + delta, 255, 255])
-
-# Threshold the HSV image to get only blue colors
-# Threshold the HSV image to get only green colors
-mask_green = cv.inRange(hsv, lower_green, upper_green)
-# Threshold for blue
-mask_blue = cv.inRange(hsv, lower_blue, upper_blue)
-mask_pink = cv.inRange(hsv, lower_pink, upper_pink)
-mask_purple = cv.inRange(hsv, lower_purple, upper_purple)
-mask_yellow = cv.inRange(hsv, lower_yellow, upper_yellow)
+colors = [46, 100, 166, 130, 28, 146]
+delta = 10
+colormask = set_masks(colors, delta)
 
 
 # Bitwise-AND mask and original image
-res = cv.bitwise_and(image, image, mask=(mask_green | mask_blue | mask_pink | mask_purple | mask_yellow))
+res = cv.bitwise_and(image, image, mask=colormask)
 print(image.shape)
 cv.imwrite("original.jpg", image)
-cv.imwrite("mask.jpg", mask_blue)
-cv.imwrite("res.jpg", res)
+cv.imwrite("res2.jpg", res)
 '''
 cv.imshow('Display Window', image)
 cv.waitKey(0)
