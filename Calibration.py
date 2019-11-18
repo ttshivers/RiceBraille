662d49f3cc8d351d674ffd4b93b313026177f2dd
collin-allen <42555339+collin-allen@users.noreply.github.com>
2019-11-18 13:17:55 -0600

    Working on getting coordinates

--- Calibration.py
+++ Calibration.py
@@ -5,7 +5,7 @@ import ar_markers as ar
 from ar_markers.marker import HammingMarker
 
 
-test_image = cv.imread("images/scanned_collin_marked.jpg")
+test_image = cv.imread("images/coordinateTransformDots1Perspective.jpg")
 
 markers = ar.detect_markers(test_image)
 print(test_image.shape)
@@ -13,7 +13,7 @@ print(markers)
 for marker in markers:
     marker.highlite_marker(test_image)
 
-cv.imwrite('images/calibrationCoordinateTest.jpg', test_image)
+cv.imwrite('images/calibrationCoordinateTestDots1.jpg', test_image)
 # cv.imshow('test_image', test_image)
 # cv.waitKey(0)
 # cv.destroyAllWindows()
